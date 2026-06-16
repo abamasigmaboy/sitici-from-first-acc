3bbc2eb6cbc7d455666e1b63c2b1f19870bbe292
abamasigmaboy <obemeobunga@gmail.com>
2026-06-16 22:58:15 +0200

    Add files via upload

---
+++ dz2-main/java.js
@@ -0,0 +1,28 @@
+//№1
+function checkAge(age) 
+{
+  return (age > 18) ? true : confirm('Родители разрешили?');
+}
+function checkAge(age)
+{
+  return (age > 18) || confirm('Родители разрешили?');
+}
+//№2
+function pow(x, n) {
+  let result = x;
+  for (let i = 1; i < n; i++) 
+  {
+    result *= x;
+  }
+  return result;
+}
+let x = prompt("x?", '');
+let n = prompt("n?", '');
+if (n >= 1 && n % 1 == 0) 
+{
+  alert( pow(x, n) );
+} 
+else 
+{
+  alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+}
